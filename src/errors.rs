9c30fb2d25652b54ed6b3082940246a3b9fd6f22
Timofey Shufletyuk <qotofey@gmail.com>
2025-11-27 19:37:13 +0300

    Created application errors

---
+++ src/errors.rs
@@ -0,0 +1,16 @@
+use std::fmt::{self, Display, Formatter};
+
+#[derive(thiserror::Error, Debug)]
+pub enum AppError {
+    UsernameIsTaken,
+    WeakPassword,
+}
+
+impl Display for AppError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            AppError::UsernameIsTaken => write!(f, "Username is taken"),
+            AppError::WeakPassword => write!(f, "Weak password"),
+        }
+    }
+}
