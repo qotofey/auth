346bfdac836c42f58d39df642fffb34aea7e1c26
Timofey Shufletyuk <qotofey@gmail.com>
2025-12-18 19:37:32 +0300

    Refactor: Add queries

--- src/app.rs
+++ src/app.rs
@@ -1,1 +1,23 @@
+pub mod queries;
 pub mod commands;
+
+#[derive(sqlx::FromRow)]
+pub struct UserCredential {
+    pub id: uuid::Uuid,
+    pub kind: Option<String>,
+    pub login: String,
+    pub confirmed_at: Option<chrono::NaiveDateTime>,
+    pub user_id: uuid::Uuid,
+    #[sqlx(rename = "login_attempts")]
+    pub failure_login_attempts: i16,
+    pub locked_until: Option<chrono::NaiveDateTime>,
+}
+
+#[derive(sqlx::FromRow)]
+pub struct UserSecret {
+    pub id: uuid::Uuid,
+    #[sqlx(try_from = "uuid::Uuid")]
+    pub user_id: String,
+    pub password_digest: String,
+}
+
---
+++ src/app/queries.rs
@@ -0,0 +1,16 @@
+use crate::{
+    errors::AppError,
+    app::{
+        UserCredential,
+        UserSecret,
+    },
+};
+
+pub trait FindUserCredentialDao {
+    fn find_user_credential_by_login(&self, login: String) -> impl std::future::Future<Output = Result<Option<UserCredential>, AppError>> + Send;
+}
+
+pub trait FindUserSecretDao {
+    // TODO: если не найдена запись - паникаовать
+    fn find_user_secret_by_user_id(&self, id: uuid::Uuid) -> impl std::future::Future<Output = Result<Option<UserSecret>, AppError>> + Send;
+}
